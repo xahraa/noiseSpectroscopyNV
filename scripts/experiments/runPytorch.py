ec318e8dd916e61b40487eb412ddfd08f254f7ce
xahraa <128670678+xahraa@users.noreply.github.com>
2024-05-21 18:50:13 +0300

    Update runPytorch.py

--- scripts/experiments/runPytorch.py
+++ scripts/experiments/runPytorch.py
@@ -9,8 +9,8 @@
 # kill `ps -e|grep runPyt|cut -d ' ' -f2`
 
 import os
-#os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID";
-#os.environ["CUDA_VISIBLE_DEVICES"]="0";
+# os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID";
+# os.environ["CUDA_VISIBLE_DEVICES"]="0";
 import sys
 import socket
 import configurations
@@ -32,7 +32,7 @@ if __name__ == '__main__':
             device = "cuda:{}".format(sys.argv[2])
         else:
             device = "cuda:0"
-        # conf = getattr(sys.modules['configurations'], sys.argv[1])
+        
         conf = eval('configurations.{}'.format(sys.argv[1]))
         startTime = datetime.now()
     
@@ -45,13 +45,13 @@ if __name__ == '__main__':
             if os.path.exists(fun.filesPath(conf)) and not (conf.has("force") and conf.force==True):
                 raise(Exception("ALREADY LAUNCHED"))
             print("======= CREATE MODEL")
-            model,optim = fun.makeModel(conf, device)
+            model, optim = fun.makeModel(conf, device)
             bestValidMetric = None
         else:
             print("======= LOAD MODEL")
-            model,optim,loadEpoch,bestValidMetric = fun.loadModel(conf, device)
+            model, optim, loadEpoch, bestValidMetric = fun.loadModel(conf, device)
             if startEpoch == -1:
-                startEpoch = loadEpoch+1
+                startEpoch = loadEpoch + 1
         print("======= LOAD DATA")
         dataloaders, _ = fun.processData(conf)
         print("======= TRAIN MODEL")
@@ -62,5 +62,7 @@ if __name__ == '__main__':
         print("End {}".format(sys.argv[1]))
         print(endTime.strftime(timeFormat))
         print("====================")
-        # notifier.sendMessage("Training of {} finished on {}".format(sys.argv[1], socket.gethostname()), "Start:\t{}\nEnd:\t{}\nDuration:\t{}".format(startTime.strftime(timeFormat),endTime.strftime(timeFormat),str(endTime-startTime).split('.', 2)[0]))
-    
+        notifier.sendMessage("Training of {} finished on {}".format(sys.argv[1], socket.gethostname()), 
+                             "Start:\t{}\nEnd:\t{}\nDuration:\t{}".format(startTime.strftime(timeFormat),
+                                                                         endTime.strftime(timeFormat),
+                                                                         str(endTime - startTime).split('.', 2)[0]))
